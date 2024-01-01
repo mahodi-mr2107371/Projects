b88adca6730579f05a83f1153cd4aa62eca81d6e
Mahodidothsp <123658483+Mahodidothsp@users.noreply.github.com>
2024-01-01 14:19:38 +0300

    Completed

--- webDev/Basic Calculator/index.js
+++ webDev/Basic Calculator/index.js
@@ -1,4 +1,10 @@
-let operator = "";
+let operator = {
+    "/":[],
+    "x":[],
+    "+":[],
+    "-":[]
+};
+let values = [];
 let ans = 0;
 function clicked(btnVar){
     switch(btnVar){
@@ -10,7 +16,7 @@ function clicked(btnVar){
         break;
         case "x": document.getElementById('input').innerHTML = document.getElementById('input').innerText.concat("x");
         break;
-        case "/": document.getElementById('input').innerHTML = document.getElementById('input').innerText.concat("/");
+        case "÷": document.getElementById('input').innerHTML = document.getElementById('input').innerText.concat("÷");
         break;
         case "+": document.getElementById('input').innerHTML = document.getElementById('input').innerText.concat("+");
         break;
@@ -18,43 +24,23 @@ function clicked(btnVar){
         break;
         case "x10": document.getElementById('input').innerHTML = document.getElementById('input').innerText.concat("x10");
         break;
-        case "=":if(seperator()!==0){document.getElementById('output').innerHTML = cal(seperator()[0],seperator()[1],operator);}else if(seperator()===1){document.getElementById("output").innerHTML = ans;}else{document.getElementById('output').innerHTML = document.getElementById('input').innerText};ans= document.getElementById('output').innerText;
+        case "=":document.getElementById('output').innerHTML = seperator();/*if(seperator()!==0){document.getElementById('output').innerHTML = cal(seperator()[0],seperator()[1],operator);}else if(seperator()===1){document.getElementById("output").innerHTML = "ans";}else{document.getElementById('output').innerHTML = document.getElementById('input').innerText};*/
         break;
-        case "ANS": document.getElementById("input").innerHTML = document.getElementById("input").innerText.concat("ANS") ;
+        case "ANS": document.getElementById("input").innerHTML = document.getElementById("input").innerText.concat(ans) ;
         break;
         default: document.getElementById('input').innerHTML = document.getElementById('input').innerText.concat(btnVar);
         break;
     }
 }
 function seperator(){
-    if(document.getElementById('input').innerText.includes("+")){
-        operator = "+";
-        return document.getElementById('input').innerText.split("+");
-    }
-    else if(document.getElementById('input').innerText.includes("-")){
-        operator = "-";
-        return document.getElementById('input').innerText.split("-");
-    }
-    else if(document.getElementById('input').innerText.includes("/")){
-        operator = "/";
-        return document.getElementById('input').innerText.split("/");
-    }
-    else if(document.getElementById('input').innerText.includes("x")){
-        operator = "x";
-        return document.getElementById('input').innerText.split("x");
-    }
-    else if(document.getElementById("input").innerText == "ANS"){
-        return 1;
-    }
-    else{
-        return 0;
-    }
-}
-function cal(val1,val2,op){
-    switch(op){
-        case "x": return (val1*1) * (val2*1);
-        case "/": return (val1*1) / (val2*1);
-        case "+": return (val1*1) + (val2*1);
-        case "-": return (val1*1) - (val2*1);
-    }
+    let docTxt = document.getElementById("input").textContent;
+    /*to use for replacing unrecognized operator
+    docTxt.replace(/÷/g,'/');
+    docTxt.replace(/x/g,"*");
+    docTxt.replace(/+/g,'+');
+    docTxt.replace(/-/g,'-');*/
+    docTxt = docTxt.replace(/x/g,"*").replace(/÷/g,'/');
+    var result = eval(docTxt);
+    ans = result*1;
+    return result;
 }
